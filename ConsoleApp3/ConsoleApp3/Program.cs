79a7bb5af2a573b841c2a3681297fa0920f55c56
Kotlel <kashtanovradmir@gmail.com>
2024-11-21 21:07:12 +0300

    Add files via upload

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        public enum Weekdays
+        {
+            Понедельник = 1,
+            Вторник,
+            Среда,
+            Четверг,
+            Пятница,
+            Суббота,
+            Воскресенье
+        }
+        static void Main(string[] args)
+        {
+            // Task 1
+            int[] num = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Введите элемент массива под номером {i}");
+                num[i] = int.Parse(Console.ReadLine());
+            }
+            for (int j = 1; j < 10; j++)
+            {
+                if (num[j] < num[j - 1])
+                {
+                    Console.WriteLine($"Число {num[j]} под номером {j}");
+                    break;
+                }
+                else if (j == 9)
+                {
+                    Console.WriteLine("Упорядоченное");
+                }
+            }    
+            // Task 2
+            try
+            {
+                Console.WriteLine("Введите номер карты");
+                int k = int.Parse(Console.ReadLine());
+                if (6 <= k && k <= 14)
+                {
+                    switch (k)
+                    {
+                        case 6:
+                            Console.WriteLine("Шестерка");
+                            break;
+                        case 7:
+                            Console.WriteLine("Семерка");
+                            break;
+                        case 8:
+                            Console.WriteLine("Восьмерка");
+                            break;
+                        case 9:
+                            Console.WriteLine("Девятка");
+                            break;
+                        case 10:
+                            Console.WriteLine("Десятка");
+                            break;
+                        case 11:
+                            Console.WriteLine("Валет");
+                            break;
+                        case 12:
+                            Console.WriteLine("Дама");
+                            break;
+                        case 13:
+                            Console.WriteLine("Король");
+                            break;
+                        case 14:
+                            Console.WriteLine("Туз");
+                            break;
+
+                        default:
+                            Console.WriteLine("Вы ввели неверное число");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Вы ввели неверное число");
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Ошибка ввода");
+            }
+            finally
+            {
+                Console.WriteLine("Отработал");
+            }
+            // Task 3
+            Console.WriteLine("Введите данные");
+            string line = Console.ReadLine();
+            switch (line.ToLower())
+            {
+                case "jabroni":
+                    Console.WriteLine("Patron Tequila");
+                    break;
+                case "school counselor":
+                    Console.WriteLine("Anything with Alcohol");
+                    break;
+                case "programmer":
+                    Console.WriteLine("Hipster Craft Beer");
+                    break;
+                case "bike gang member":
+                    Console.WriteLine("Moonshine");
+                    break;
+                case "Politician":
+                    Console.WriteLine("Your tax dollars");
+                    break;
+                case "rapper":
+                    Console.WriteLine("Cristal");
+                    break;
+                default:
+                    Console.WriteLine("Beer");
+                    break;
+            }
+            // Task 4
+            Console.WriteLine("Введите номер дня недели от 1 до 7");
+            int dday = int.Parse(Console.ReadLine());
+            if (dday > 0 && dday < 8)
+            {
+                Console.WriteLine($"{dday} день недели - {(Weekdays)dday}");
+            }
+            else
+            {
+                Console.WriteLine("Нет такого дня недели");
+            }
+            // Task 5
+            Console.WriteLine("Введите длину массива");
+            int numerus = int.Parse(Console.ReadLine());
+            string[] arraytoys = new string[numerus];
+            for (int p = 0; p < numerus; p++)
+            {
+                Console.WriteLine($"Введите элемент массива под номером {p}");
+                arraytoys[p] = Console.ReadLine();
+            }
+            int bag = 0;
+            foreach (string elem in arraytoys)
+            {
+                if (elem.ToLower() == "hello kitty" || elem.ToLower() == "barbie doll")
+                {
+                    bag++;
+                }
+            }
+            Console.WriteLine($"{bag} элементов в сумке");
+        }
+    }
+}
---
+++ ConsoleApp3/lab/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 4.1
+            int day = 0;
+            Console.WriteLine("Введите число от 1 до 365");
+            day = int.Parse(Console.ReadLine());
+            DateTime date = new DateTime(1, 1, 1);
+            Console.WriteLine($"{day} соответствует: {date.AddDays(day - 1).ToString("dd MMMM")}");
+            // 4.2
+            int dayexc = 0;
+            while (true)
+            {
+                Console.WriteLine("Введите число от 1 до 365");
+                dayexc = int.Parse(Console.ReadLine());
+                if (dayexc <= 365 && dayexc > 0) break;
+                else Console.WriteLine("Ошибка");
+            }
+
+            DateTime dateexc = new DateTime(1, 1, 1);
+            Console.WriteLine($"{dayexc} соответствует: {dateexc.AddDays(dayexc - 1).ToString("dd MMMM")}");
+            // 4.1
+            int year = 0;
+            Console.WriteLine("Введите год");
+            year = int.Parse(Console.ReadLine());
+            if ((year % 4 == 0 && (year % 100 != 0)) || (year % 400 == 0))
+            {
+                int dayleap = 0;
+                while (true)
+                {
+                    Console.WriteLine("Введите число от 1 до 366");
+                    dayleap = int.Parse(Console.ReadLine());
+                    if (dayleap <= 366 && dayleap > 0) break;
+                    else Console.WriteLine("Ошибка");
+                }
+
+                DateTime dateleap = new DateTime(year, 1, 1);
+                Console.WriteLine($"{dayleap} соответствует: {dateleap.AddDays(dayleap - 1).ToString("dd MMMM yyyy")}");
+            }
+            else
+            {
+                int daynotleap = 0;
+                while (true)
+                {
+                    Console.WriteLine("Введите число от 1 до 365");
+                    daynotleap = int.Parse(Console.ReadLine());
+                    if (daynotleap <= 365 && daynotleap > 0) break;
+                    else Console.WriteLine("Ошибка");
+                }
+
+                DateTime datenotleap = new DateTime(year, 1, 1);
+                Console.WriteLine($"{daynotleap} соответствует: {datenotleap.AddDays(daynotleap - 1).ToString("dd MMMM yyyy")}");
+            }
+        }
+    }
+}
